e812879b69dbffdfabd60288795880536bbcf043
sarangchoi11 <sarangchoi11@gmail.com>
2024-10-04 00:35:59 +0900

    Figured out dotenv problem

--- backpack/memo.py
+++ backpack/memo.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import os
-#from openai import OpenAI
+from openai import OpenAI
 from dotenv import load_dotenv
 
-load_dotenv('../.env')
+load_dotenv('.env')
 
 st.title("Memo 📝")
 
-#client = OpenAI(
-    #api_key=os.getenv("OPENAI_API_KEY")
-#)
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
 
-book_choices = ["book1", "book2", "book3"]
+book_choices = ["book1", "book2", "book3"] # later, get titles from db
 book_select = st.selectbox("어떤 책을 위한 메모인가요?", book_choices)
 
 memo_input = st.text_area("Memo")
@@ -19,3 +19,4 @@ memo_input = st.text_area("Memo")
 if st.button("Save"):
     st.write(memo_input)
 
+# save memo in the db
